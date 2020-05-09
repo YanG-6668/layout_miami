6f0996b45ccfe9430cd9b0b0f36439d829ff5021
YanG-6668 <goch.yannn@gmail.com>
2020-05-09 14:32:35 +0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+$(document).ready(function() {
+  $('a[href^="#"]').click(function() {
+    const target = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(target).offset().top
+    }, 1500);
+  });
+});
+
+$(".header__icon_bars, .mobile__icon").click(function() {
+  $('.mobile').slideToggle(500);
+});
